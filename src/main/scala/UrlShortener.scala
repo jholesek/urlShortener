dd7f86e54c8c2726eade58bd3646c147e0f1e80b
Jakob Holesek <jholesek@gmail.com>
2025-11-16 15:47:30 +0000

    Started the shortehUrl function.

--- src/main/scala/UrlShortener.scala
+++ src/main/scala/UrlShortener.scala
@@ -1,10 +1,14 @@
 object UrlShortener extends cask.MainRoutes {
+        def shortenUrl(url: String): String = {
+                url.reverse
+        }
+
         @cask.staticResources("/static")
         def staticEndpoint(): String="."
 
         @cask.post("/shorten")
         def shorten(request: cask.Request) = {
-                request.text().reverse
+                shortenUrl(request.text())
         }
 
         initialize()
