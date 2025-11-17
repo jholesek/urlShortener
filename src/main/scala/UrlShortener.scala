f6cbe23e45c9598c7f4b13a54f14224be9c03977
Jakob Holesek <jholesek@gmail.com>
2025-11-17 21:10:46 +0000

    Added initial redirect method.

--- src/main/scala/UrlShortener.scala
+++ src/main/scala/UrlShortener.scala
@@ -6,11 +6,17 @@ object UrlShortener extends cask.MainRoutes {
                 val key= scala.util.Random.alphanumeric.take(10).mkString
                 store(key) = url
 
-                "http://localhost:8080/"+key
+                "http://localhost:8080/link/"+key
         }
 
         @cask.staticResources("/static")
         def staticEndpoint(): String="."
+
+        @cask.get("/link")
+        def forwardReq(segments: cask.RemainingPathSegments): String = {
+                store(segments.value.mkString)
+                //segments.value.mkString
+        }
 
         @cask.post("/shorten")
         def shorten(request: cask.Request) = {
